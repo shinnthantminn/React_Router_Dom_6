7f330c252d581a6a62b3f25d019882757b415332
shinnthantminn <85192394+shinnthantminn@users.noreply.github.com>
2022-05-17 12:33:10 +0630

    replace

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,7 +4,11 @@ import { NavLink } from 'react-router-dom'
 function Nav() {
   return (
     <div>
-      <NavLink to="/" className={({ isActive }) => (isActive ? 'one' : 'two')}>
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? 'one' : 'two')}
+        replace //သူနောက်မှာဘယ်သူမှ မရှိအောင်လုပ်တာပါ backword ပြန်လုပ်လို့မရအောင်လုပ်တာပါ
+      >
         Home
       </NavLink>
       <NavLink
